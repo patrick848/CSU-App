707870dfe728b5837d56c1347e7bdee817191eb4
Patrick Mahoney <patrick64@gmail.com>
2012-09-10 19:27:26 -0400

    Removed old drawables and moved them into a folder in root direcrtory.  These need to be redrawn as vectors...

--- gen/com/patmahoneyJR/csu/vikings/R.java
+++ gen/com/patmahoneyJR/csu/vikings/R.java
@@ -11,17 +11,8 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int calendar_icon=0x7f020000;
-        public static final int dining_icon=0x7f020001;
-        public static final int fightsong_icon=0x7f020002;
-        public static final int fouroneone_icon=0x7f020003;
-        public static final int ic_action_search=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
-        public static final int map_icon=0x7f020006;
-        public static final int news_icon=0x7f020007;
-        public static final int parking_icon=0x7f020008;
-        public static final int socialnetwork_icon=0x7f020009;
-        public static final int technology_icon=0x7f02000a;
+        public static final int ic_action_search=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
         public static final int calendarButton=0x7f070000;
