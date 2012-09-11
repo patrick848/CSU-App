ac8c723f8d57342ec1a585abfc39ca3263a16c77
Patrick Mahoney <patrick64@gmail.com>
2012-09-11 12:10:40 -0400

    Added skeletons of the new activity classes

---
+++ src/com/patmahoneyJR/csu/vikings/AcademicCalendar.java
@@ -0,0 +1,13 @@
+package com.patmahoneyJR.csu.vikings;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AcademicCalendar extends Activity {
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+	}
+}
---
+++ src/com/patmahoneyJR/csu/vikings/Parking.java
@@ -0,0 +1,13 @@
+package com.patmahoneyJR.csu.vikings;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Parking extends Activity {
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+	}
+}
