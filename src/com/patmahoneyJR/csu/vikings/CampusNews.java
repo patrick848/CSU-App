563ab4170e2d8e5fc429f4a64ffde8b2d7d2f5fd
Patrick Mahoney <patrick64@gmail.com>
2012-09-12 15:13:54 -0400

    Removed setContentView

--- src/com/patmahoneyJR/csu/vikings/CampusNews.java
+++ src/com/patmahoneyJR/csu/vikings/CampusNews.java
@@ -1,13 +1,12 @@
 package com.patmahoneyJR.csu.vikings;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 
-public class CampusNews extends Activity {
+public class CampusNews extends ListActivity {
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.news_activity);
 	}
 }
