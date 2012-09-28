4441d21f19d2c37ff1146c0f28db276500d4ef95
Patrick Mahoney <patrick64@gmail.com>
2012-09-27 20:06:02 -0400

    Pulls news successfully now, with no extra fluff!
    Does it look good? No.  Fix that later.

--- src/com/patmahoneyJR/csu/vikings/CampusNews.java
+++ src/com/patmahoneyJR/csu/vikings/CampusNews.java
@@ -20,27 +20,35 @@ public class CampusNews extends ListActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 		
+        // Make a new AsyncTask to fetch news
         GetNewsItemsTask getNews = new GetNewsItemsTask();
 		getNews.execute();
 	}
 	
 	private class GetNewsItemsTask extends AsyncTask<Void, Void, ArrayList<String>> {
 		
+		// Connects using Jsoup, gets all "p" tagged elements from URL,
+		// then gets the "a" tag child elements of those, then if those
+		// have text, adds them to an ArrayList called items which is returned
+		// later.
 		protected ArrayList<String> doInBackground(Void... urls) {
 			ArrayList<String> items = new ArrayList<String>();
 			try {
 				Document doc = Jsoup.connect(URL).get();
-				for (Element e : doc.getElementsByClass("copy")) {
-					if(e.getElementsByTag("a").text().trim().length() != 0)			
+				for (Element e : doc.getElementsByTag("p")) {
+					if((e.getElementsByTag("a")).hasText()) // Checks is it has text
 						items.add(e.getElementsByTag("a").text());
+					else break; // break loop before we pick up crappy links to stuff we don't care about...
 				}
+				// ROBUST-IFY THAT SHIT
 			} catch (Exception e) {		
 				Toast.makeText(getApplicationContext(), "Couldn't fetch articles, try again later.",
-						Toast.LENGTH_SHORT).show();
+						Toast.LENGTH_SHORT).show();  
 			}
 			return items;
 		}
 		
+		// Called immediately after doInBackground and receives items, then updates UI to reflect the new data!
 		protected void onPostExecute(ArrayList<String> items) {
 			
 			setListAdapter(new ArrayAdapter<String>(getBaseContext(),
