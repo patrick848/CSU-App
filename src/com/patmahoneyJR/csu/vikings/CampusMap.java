7a2040726b1f96b5e5e5f2117beb03dbd4a9cb16
Patrick Mahoney <patrick64@gmail.com>
2012-09-30 15:03:00 -0400

    CampusMap now hones in on Student Center.

--- src/com/patmahoneyJR/csu/vikings/CampusMap.java
+++ src/com/patmahoneyJR/csu/vikings/CampusMap.java
@@ -1,13 +1,33 @@
 package com.patmahoneyJR.csu.vikings;
 
-import android.app.Activity;
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
 import android.os.Bundle;
 
-public class CampusMap extends Activity {
+public class CampusMap extends MapActivity {
+	
+	private MapView map;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_activity);
+        map = (MapView) findViewById(R.id.map);
+        map.setBuiltInZoomControls(true);
+        map.getController().setCenter(getPoint(41.502217,-81.675411));
+        map.getController().setZoom(20);
 	}
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	private GeoPoint getPoint(double lat, double lon) {
+	    return(new GeoPoint((int)(lat*1000000.0),
+	                          (int)(lon*1000000.0)));
+	  }
 }
