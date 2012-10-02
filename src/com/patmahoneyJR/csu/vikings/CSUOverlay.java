ed00c83174a260dbee3787fbe6f5431cb925b9bf
Patrick Mahoney <patrick64@gmail.com>
2012-10-02 10:15:52 -0400

    Markers now work.  Created on center of map, currently uses temporary Android Marker.

--- src/com/patmahoneyJR/csu/vikings/CSUOverlay.java
+++ src/com/patmahoneyJR/csu/vikings/CSUOverlay.java
@@ -1,5 +1,35 @@
 package com.patmahoneyJR.csu.vikings;
 
-public class CSUOverlay {
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.OverlayItem;
+
+public class CSUOverlay extends ItemizedOverlay<OverlayItem> {
+	
+	private List<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+
+	public CSUOverlay(Drawable marker, Context context) {
+		super(boundCenterBottom(marker));
+	}
+	
+	public void addOverlay(OverlayItem overlay) {
+	    mOverlays.add(overlay);
+	    populate();
+	}
+
+	@Override
+	protected OverlayItem createItem(int i) {
+		return mOverlays.get(i);
+	}
+
+	@Override
+	public int size() {
+		return mOverlays.size();
+	}
 
 }
--- src/com/patmahoneyJR/csu/vikings/CampusMap.java
+++ src/com/patmahoneyJR/csu/vikings/CampusMap.java
@@ -1,24 +1,19 @@
 package com.patmahoneyJR.csu.vikings;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
-import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.Overlay;
 import com.google.android.maps.OverlayItem;
-import com.google.android.maps.MyLocationOverlay;
-
-import android.graphics.drawable.Drawable;
-import android.os.Bundle;
-import android.util.Log;
 
 public class CampusMap extends MapActivity {
 	
 	private MapView map;
-	private MyLocationOverlay me;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,12 +26,14 @@ public class CampusMap extends MapActivity {
         map.getController().setCenter(getPoint(extras.getDouble("lat"), extras.getDouble("lon")));
         map.getController().setZoom(19);
         
+        List<Overlay> mapOverlays = map.getOverlays();
         Drawable marker = getResources().getDrawable(R.drawable.androidmarker);
         marker.setBounds(0, 0, marker.getIntrinsicWidth(),
         		marker.getIntrinsicHeight());
-        map.getOverlays().add(new SitesOverlay(marker));
-        me=new MyLocationOverlay(this, map);
-        map.getOverlays().add(me);
+        CSUOverlay overlay = new CSUOverlay(marker, this);
+        OverlayItem overlayItem = new OverlayItem(map.getMapCenter(), null, null);
+        overlay.addOverlay(overlayItem);
+        mapOverlays.add(overlay);
         
 	}
 
@@ -50,31 +47,4 @@ public class CampusMap extends MapActivity {
 	    return(new GeoPoint((int)(lat*1000000.0),
 	                          (int)(lon*1000000.0)));
 	  }
-	
-	private class SitesOverlay extends ItemizedOverlay<OverlayItem> {
-		
-		private List<OverlayItem> items=new ArrayList<OverlayItem>();
-	    
-	    public SitesOverlay(Drawable marker) {
-	    	
-	      super(marker);
-	      
-	      boundCenterBottom(marker);
-	      
-	      items.add(new OverlayItem(map.getMapCenter(), "hi", "yo"));
-	      
-	      populate();
-	    }
-
-		@Override
-		protected OverlayItem createItem(int i) {
-			return(items.get(i));
-		}
-
-		@Override
-		public int size() {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-	}
 }
