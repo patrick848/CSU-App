4bffc8e19fb04e7ff5cbe315d25a63f2f0be22ae
Patrick Mahoney <patrick64@gmail.com>
2012-10-10 15:15:57 -0400

    Added basic list elements

--- src/com/patmahoneyJR/csu/vikings/CSUTechnology.java
+++ src/com/patmahoneyJR/csu/vikings/CSUTechnology.java
@@ -2,11 +2,19 @@ package com.patmahoneyJR.csu.vikings;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 public class CSUTechnology extends ListActivity {
+	
+	String[] techItems = {"Call Center", "Connection Center",
+			"General Computer Labs", "Mobile Campus",
+			"Mobile Email", "Password Reset", "Wireless" };
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setListAdapter(new ArrayAdapter<String>(this,
+        		R.layout.simple_list_item_1,
+        		techItems));
 	}
 }
