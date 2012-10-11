af807837d8e728db561c3057e97067cffef11638
Patrick Mahoney <patrick64@gmail.com>
2012-10-11 14:39:09 -0400

    Turned in to a ListView and Call Center works

--- src/com/patmahoneyJR/csu/vikings/CSUTechnology.java
+++ src/com/patmahoneyJR/csu/vikings/CSUTechnology.java
@@ -1,8 +1,11 @@
 package com.patmahoneyJR.csu.vikings;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class CSUTechnology extends ListActivity {
 	
@@ -10,11 +13,24 @@ public class CSUTechnology extends ListActivity {
 			"General Computer Labs", "Mobile Campus",
 			"Mobile Email", "Password Reset", "Wireless" };
 	
+	String[] computerLabs = {"BU 23", "FT 103", "SH 128", "UR 39/40"};
+	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setListAdapter(new ArrayAdapter<String>(this,
         		R.layout.simple_list_item_1,
         		techItems));
+	}
+	
+	public void onListItemClick(ListView parent, View v, 
+			int position, long id) {
+		
+		if(techItems[position].equals("Call Center")) {
+			Intent callCenter = new Intent(this, CallCenter.class);
+			startActivity(callCenter);
+		}
+			
+		
 	}
 }
