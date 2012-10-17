eb19a487d82b744ca8088227b92f68928a11fa0a
Patrick Mahoney <patrick64@gmail.com>
2012-10-16 21:35:09 -0400

    Set to open social.html

--- src/com/patmahoneyJR/csu/vikings/SocialNetwork.java
+++ src/com/patmahoneyJR/csu/vikings/SocialNetwork.java
@@ -2,12 +2,17 @@ package com.patmahoneyJR.csu.vikings;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class SocialNetwork extends Activity {
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.social_activity);
+        setContentView(R.layout.webview);
+        
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.loadUrl("file:///android_asset/social.html");
+        
 	}
 }
