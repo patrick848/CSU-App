c8b14cb96ba0f9ff4cc5e2216f0d5054c4842fbb
Patrick Mahoney <patrick64@gmail.com>
2012-10-16 23:26:30 -0400

    Renamed/changed to Dining

---
+++ src/com/patmahoneyJR/csu/vikings/Dining.java
@@ -0,0 +1,12 @@
+package com.patmahoneyJR.csu.vikings;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class Dining extends ListActivity {
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+	}
+}
