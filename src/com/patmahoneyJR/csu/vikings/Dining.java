e5fac0a337ecf5a28b7d9a9aa25fe5c9b3093b18
Patrick Mahoney <patrick64@gmail.com>
2012-11-01 00:39:15 -0400

    Added in base layout for class, not currently in use.

--- src/com/patmahoneyJR/csu/vikings/Dining.java
+++ src/com/patmahoneyJR/csu/vikings/Dining.java
@@ -2,11 +2,26 @@ package com.patmahoneyJR.csu.vikings;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Dining extends ListActivity {
 	
+	String[] dining;
+	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState); 
+        dining = getResources().getStringArray(R.array.dining);
+        setListAdapter(new ArrayAdapter<String>(this,
+        		R.layout.simple_list_item_1,
+        		dining));
+        
+	}
+	
+	public void onListItemClick(ListView parent, View v, 
+			int position, long id) {
+		
 	}
 }
